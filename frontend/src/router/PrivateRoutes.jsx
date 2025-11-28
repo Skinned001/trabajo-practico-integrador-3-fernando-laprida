f4e780b2ede2793d0540d38a3caf9ce455c0bbb2
Laprida <psmathe001@gmail.com>
2025-11-28 11:50:55 -0300

    rutas publicas y privadas creadas

---
+++ frontend/src/router/PrivateRoutes.jsx
@@ -0,0 +1,14 @@
+import { Navigate, Outlet } from "react-router-dom";
+import { Navbar } from "../components/Navbar";
+
+export const PrivateRoutes = () => {
+    const isLogged = localStorage.getItem("isLogged");
+    return isLogged ? (
+        <>
+         <Navbar/>
+         <Outlet/>
+        </>
+    ) : (
+        <Navigate to="/Login" />
+    );
+};
